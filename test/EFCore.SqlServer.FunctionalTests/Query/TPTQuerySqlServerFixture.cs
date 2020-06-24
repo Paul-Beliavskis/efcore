1291bb1c004b1421ae598faec4930bf1904ca77f
Smit Patel <smitpatel@users.noreply.github.com>
2020-06-23 18:11:13 -0700

    Test: Add test base for TPT in InheritanceQueryTestBase
    Part of #2266

--- test/EFCore.SqlServer.FunctionalTests/Query/TPTQuerySqlServerFixture.cs
+++ test/EFCore.SqlServer.FunctionalTests/Query/TPTQuerySqlServerFixture.cs
@@ -1,11 +1,11 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using Microsoft.EntityFrameworkCore.TestUtilities;
 
 namespace Microsoft.EntityFrameworkCore.Query
 {
-    public class InheritanceSqlServerFixture : InheritanceRelationalFixture
+    public class TPTQuerySqlServerFixture : TPTQueryRelationalFixture
     {
         protected override ITestStoreFactory TestStoreFactory => SqlServerTestStoreFactory.Instance;
     }
